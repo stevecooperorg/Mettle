37121d81b64d6b3a1af0585b37b6a685a4f3aeb2
Steve Cooper <steve@stevecooper.org>
2013-02-24 02:24:36 +0000

    a bit more progress. nothing special

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 var fs = require('fs');
+var PEG = require("pegjs");
 
-fs.readFile('../examples/modern-combat/skills.txt', 'utf-8', function(err, data) {
+var grammarFile = "skillgrammar.txt";
+var skillFile = "../examples/modern-combat/skills.txt";
+
+function createParser(grammar) {
+	var parser = PEG.buildParser(grammar, { cache: true, trackLineAndColumn: true });
+	fs.readFile(skillFile, 'utf-8', readSkillFile);
+}
+
+function readGrammarFile(err, data) {
+	grammarContent = data;
+	createParser(data)
+}
+
+function readSkillFile(err, data) {
 	console.log(data);
-});
+}
+
+fs.readFile(grammarFile, 'utf-8', readGrammarFile);
+
+// fs.readFile('../examples/modern-combat/skills.txt', 'utf-8', function(err, data) {
+// 	console.log(data);
+// });
