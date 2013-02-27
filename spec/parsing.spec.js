165ebd5474b77092ed2f474df669cc9e449b9986
Steve Cooper <steve@stevecooper.org>
2013-02-27 23:29:19 +0000

    started a lexer for skill files

---
+++ spec/parsing.spec.js
@@ -0,0 +1,88 @@
+"use strict"
+var parsing = require('../src/parsing');
+
+
+describe("the lexer", function() {
+	var lexer;
+
+	beforeEach(function() {
+		lexer = new parsing.Lexer();	
+	})
+
+	it("understands unquoted skill names", function() {
+		var tokens = lexer.tokenize("firearms running");
+		var expected = [
+			{
+				content: "firearms",
+				type: "skill",
+				position: 0
+			},
+			{
+				content: "running", 
+				type: "skill",
+				position: 9
+			}
+		];
+		expect(tokens).toEqual(expected);
+	});
+
+	it("should understand quote-wrapped skill names", function() {
+		var tokens = lexer.tokenize("'library use'");
+
+		var expected = [
+			{
+				content: "library use",
+				type: "skill",
+				position: 0
+			}
+		];
+		expect(tokens).toEqual(expected);
+	});
+
+	it("should understand escaped quotes in quote-wrapped skill names", function() {
+		var input = "'England\'s pride'";
+		var tokens = lexer.tokenize(input);
+		var expected = [
+			{
+				content: "England\'s pride",
+				type: "skill",
+				position: 0
+			}
+		];
+		expect(tokens).toEqual(expected);
+	});
+	
+	it("should understand multiple quoted names", function() {
+		var input = "'first' 'second'";
+		var tokens = lexer.tokenize(input);
+		var expected = [
+			{
+				content: "first",
+				type: "skill",
+				position: 0
+			},
+			{
+				content: "second",
+				type: "skill",
+				position: 8
+			}
+		];
+		expect(tokens).toEqual(expected);
+	});
+});
+
+// describe("the parser", function() {
+
+// 	var parser;
+// 	beforeEach(function() {
+// 		parser = new parsing.Parser();
+// 	});
+
+// 	it("should return an empty graph for no input", function() {
+// 		var graph = parser.parse([]);
+// 		expect(graph).toEqual({
+// 			nodes: [],
+// 			edges: []
+// 		});
+// 	});
+// });
---
+++ src/parsing.js
@@ -0,0 +1,42 @@
+"use strict";
+(function(parsing){
+    var canto34 = require('../../Canto34/src/canto34');
+
+	// parsing.Parser = function() {
+	// };
+
+	// parsing.Parser.prototype.parse = function(tokens) {
+	// 	var graph = {
+	// 		nodes: [],
+	// 		edges: []
+	// 	};
+
+	// 	return graph;
+	// }
+
+	parsing.Lexer = function() {
+		canto34.Lexer.call(this);
+		this.addTokenType(canto34.StandardTokenTypes.whitespace());
+		
+		// skills can be names like "running"
+		this.addTokenType({ 
+			name: "skill", 
+			regexp: /^[a-z]+/
+		});
+
+		// skills include other characters if single-quoted; like "'library
+		// use'", use \' to escape single-quotes.
+		this.addTokenType({ 
+			name: "skill", 
+			regexp: /^'(x|[^'])+'/, 
+			interpreter: function(content) {
+				var insideQuotes = content.substring(1, content.length-1);
+				var escapeQuotes = insideQuotes.replace("\'", "'");
+				return escapeQuotes;
+			} 
+		});
+	};
+
+	parsing.Lexer.prototype = new canto34.Lexer();
+
+})(typeof exports === 'undefined'? this['parsing']={}: exports);
