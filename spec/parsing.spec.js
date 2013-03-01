8c9f4528f11d34c2b467afd5c90602598606230a
Steve Cooper <steve@stevecooper.org>
2013-03-01 00:48:12 +0000

    parser finished.
    And git hated a little

--- spec/parsing.spec.js
+++ spec/parsing.spec.js
@@ -1,88 +1,153 @@
 "use strict"
 var parsing = require('../src/parsing');
-
+var canto34 = require('../../Canto34/src/canto34');
 
 describe("the lexer", function() {
 	var lexer;
 
 	beforeEach(function() {
-		lexer = new parsing.Lexer();	
-	})
+		lexer = new parsing.Lexer();
+		this.addMatchers(canto34.Jasmine.matchers);
+	});
+
 
 	it("understands unquoted skill names", function() {
 		var tokens = lexer.tokenize("firearms running");
-		var expected = [
-			{
-				content: "firearms",
-				type: "skill",
-				position: 0
-			},
-			{
-				content: "running", 
-				type: "skill",
-				position: 9
-			}
-		];
-		expect(tokens).toEqual(expected);
+		expect(tokens).toHaveTokenTypes(["skill", "skill"]);
+		expect(tokens).toHaveTokenContent(["firearms", "running"]);
 	});
 
 	it("should understand quote-wrapped skill names", function() {
 		var tokens = lexer.tokenize("'library use'");
+		expect(tokens).toHaveTokenTypes(["skill"]);
+		expect(tokens).toHaveTokenContent(["library use"]);
 
-		var expected = [
-			{
-				content: "library use",
-				type: "skill",
-				position: 0
-			}
-		];
-		expect(tokens).toEqual(expected);
 	});
 
 	it("should understand escaped quotes in quote-wrapped skill names", function() {
-		var input = "'England\'s pride'";
+		var input = "'England\\'s pride'";
 		var tokens = lexer.tokenize(input);
-		var expected = [
-			{
-				content: "England\'s pride",
-				type: "skill",
-				position: 0
-			}
-		];
-		expect(tokens).toEqual(expected);
+		expect(tokens).toHaveTokenTypes(["skill"]);
+		expect(tokens).toHaveTokenContent(["England\\'s pride"]);
 	});
 
 	it("should understand multiple quoted names", function() {
 		var input = "'first' 'second'";
 		var tokens = lexer.tokenize(input);
-		var expected = [
-			{
-				content: "first",
-				type: "skill",
-				position: 0
-			},
-			{
-				content: "second",
-				type: "skill",
-				position: 8
-			}
-		];
-		expect(tokens).toEqual(expected);
+		expect(tokens).toHaveTokenTypes(["skill", "skill"]);
+		expect(tokens).toHaveTokenContent(["first", "second"]);		
+	});
+
+	it("should understand integers", function() {
+		var input = "-123 456";
+		var tokens = lexer.tokenize(input);
+		expect(tokens).toHaveTokenTypes(["integer", "integer"]);
+		expect(tokens).toHaveTokenContent([-123, 456]);
+	});
+
+	it("should understand full stops and colons", function() {
+		var input = ".:";
+		var tokens = lexer.tokenize(input);
+		expect(tokens).toHaveTokenTypes(["period", "colon"]);
+	});
+
+	it("should treat newlines as whitespace", function() {
+		var input = "  \r\n  ";
+		var tokens = lexer.tokenize(input);
+		expect(tokens).toEqual([]);
+	});
+
+	it("should recognise all tokens in a complete sentence", function() {
+		var input = "firearms. running: 'foot injury' 7."
+		var tokens = lexer.tokenize(input);
+		expect(tokens).toHaveTokenTypes(["skill", "period", "skill", "colon", "skill", "integer", "period"])
 	});
 });
 
-// describe("the parser", function() {
+describe("the parser", function() {
+	
+	it("should parse discrete skill names", function() {
+		var input = "running.";
+		var tokens = new parsing.Lexer().tokenize(input);
+		var parser = new parsing.Parser(tokens);
+		var graph = parser.skill();
+		expect(graph).toEqual({
+			nodes: [ { name:"running"} ],
+			edges: []
+		});
+	});
+	
+	it("should parse starred skill names", function() {
+		var input = "running*.";
+		var tokens = new parsing.Lexer().tokenize(input);
+		var parser = new parsing.Parser(tokens);
+		var graph = parser.skill();
+		expect(graph).toEqual({
+			nodes: [ { name:"running", starred:true} ],
+			edges: []
+		});
+	});
 
-// 	var parser;
-// 	beforeEach(function() {
-// 		parser = new parsing.Parser();
-// 	});
+	it("should parse skill names with links", function() {
+		var input = "coordination: dexterity 5, quickdraw 6, brawl 7.";
+		var tokens = new parsing.Lexer().tokenize(input);
+		var parser = new parsing.Parser(tokens);
+		var graph = parser.skill();
+		expect(graph).toEqual({
+			nodes: [ 
+				{ name:"coordination" },
+				{ name:"dexterity" },
+				{ name:"quickdraw" },
+				{ name:"brawl" }
+			],
+			edges: [
+				{ 
+					source: "coordination", 
+					destination:"dexterity", 
+					strength: 5
+				},
+				{ 
+					source: "coordination", 
+					destination:"quickdraw", 
+					strength: 6
+				},
+				{ 
+					source: "coordination", 
+					destination:"brawl", 
+					strength: 7
+				}
+			]
+		});
+	});
 
-// 	it("should return an empty graph for no input", function() {
-// 		var graph = parser.parse([]);
-// 		expect(graph).toEqual({
-// 			nodes: [],
-// 			edges: []
-// 		});
-// 	});
-// });
+	it("should parse a set of skills", function() {
+		var input = "strength. dexterity. constitution.";
+		var tokens = new parsing.Lexer().tokenize(input);
+		var parser = new parsing.Parser(tokens);
+		var graph = parser.skillList();
+		expect(graph).toEqual({
+			nodes: [ 
+				{ name:"strength" },
+				{ name:"dexterity" },
+				{ name:"constitution" }
+			],
+			edges: []
+		});
+	});
+
+	it("should return an empty graph for no input", function() {
+		var parser = new parsing.Parser([]);
+		var graph = parser.skillList();
+		expect(graph).toEqual({
+			nodes: [],
+			edges: []
+		});
+	});
+
+	if("should parse the modern combat example", function() {
+		var input = "dexterity.\r\nsize.\r\nfitness.\r\n\r\n# derived stats\r\ncoordination: dexterity 5.\r\n\r\n\r\n#\r\n# Guns!\r\n#\r\nfirearms: coordination 5.\r\nhandgun: firearms 5, quickdraw 5.\r\nrifle: firearms 5.\r\nquickdraw*: coordination 5.\r\n\r\n#\r\n# Brawling\r\n#\r\nkick: brawl 8, running 2.\r\nparry: coordination 5.\r\npunch: brawl 8.\r\nbrawl: dexterity 5, fitness 5, size 5.\r\n\r\n# combat styles\r\nKarate: punch 7, kick 6, fitness 5.\r\nQueensbury rules: punch 9, fitness 8.\r\n\r\n\r\n#\r\n# Melee Weapons\r\n#\r\nmelee: size 5, fitness 5.\r\nclub: melee 5.\r\nsword: melee 5.\r\nshield: melee 5.\r\nspear: melee 5.\r\n\r\n\r\n#\r\n# running away\r\n# \r\nrunning: fitness 5.\r\nsprint: running 8.\r\ndodge: coordination 5.\r\nhorseriding: coordination 5.\r\n\r\n#\r\n# knowledge\r\n#\r\ntactics.\r\n\r\n#\r\n# Professions\r\n#\r\nknight*: sword 8, shield 4, horseriding 8, lance 7.\r\nswat team*: rifle 9, brawl 6, tactics 6.\r\nprofessional boxer*: Queensbury rules 10.\r\ncowboy*: quickdraw 9, horseriding 7, handgun 5.";
+		var tokens = new parsing.Lexer().tokenize(input);
+		var parser = new parsing.Parser(tokens);
+		parser.skillList();
+	});
+});
--- src/parsing.js
+++ src/parsing.js
@@ -2,22 +2,79 @@
 (function(parsing){
     var canto34 = require('../../Canto34/src/canto34');
 
-	// parsing.Parser = function() {
-	// };
+	parsing.Parser = function(tokens) {
+    	canto34.Parser.call(this);
+		this.initialize(tokens);
+		this.result = {
+			nodes: [],
+			edges: []
+		};
+	};
 
-	// parsing.Parser.prototype.parse = function(tokens) {
-	// 	var graph = {
-	// 		nodes: [],
-	// 		edges: []
-	// 	};
+	parsing.Parser.prototype = new canto34.Parser();
 
-	// 	return graph;
-	// }
+	parsing.Parser.prototype._addSkill = function(skillName) {
+		var skill = { 
+			name:skillName
+		};
+		this.result.nodes.push(skill);
+		return skill;
+	};
+
+	parsing.Parser.prototype.skill = function() {
+		if (this.la1("skill")) {
+			var skillName = this.match("skill").content;
+			var skill = this._addSkill(skillName);
+			var starred = false;
+			if (this.la1("star")) {
+				this.match("star");
+				skill.starred = true;
+			}
+			if (this.la1("colon")) {
+				this.match("colon");
+				var done = false;
+				while(!done) {
+					var secondarySkillName = this.match("skill").content;
+					var strength = this.match("integer").content;
+					var secondarySkill = this._addSkill(secondarySkillName);
+					var edge = {
+						source: skill.name,
+						destination: secondarySkill.name,
+						strength: strength
+					};
+					this.result.edges.push(edge);
+
+					if (this.la1("comma")) {
+						this.match("comma");
+					} else {
+						done = true;
+					}
+				}
+			}
+			this.match("period");
+		}
+		return this.result;
+	};
+
+	parsing.Parser.prototype.skillList = function() {
+		while(!this.eof() && this.la1("skill")) {
+			this.skill();
+		}
+		return this.result;
+	};
 
 	parsing.Lexer = function() {
 		canto34.Lexer.call(this);
-		this.addTokenType(canto34.StandardTokenTypes.whitespace());
 		
+		this.addTokenType({
+			name: "line comment",
+			regexp: /^\#.*?\r\n/,
+			ignore: true
+		});
+
+		this.addTokenType(canto34.StandardTokenTypes.whitespaceWithNewlines());
+		this.addTokenType(canto34.StandardTokenTypes.integer());
+
 		// skills can be names like "running"
 		this.addTokenType({ 
 			name: "skill", 
@@ -28,14 +85,32 @@
 		// use'", use \' to escape single-quotes.
 		this.addTokenType({ 
 			name: "skill", 
-			regexp: /^'(\'|[^'])+'/, 
+			regexp: /^'(\\'|[^'])+'/, 
 			interpreter: function(content) {
 				var insideQuotes = content.substring(1, content.length-1);
 				var escapeQuotes = insideQuotes.replace("\'", "'");
 				return escapeQuotes;
 			} 
 		});
-	};
+		this.addTokenType({
+			name: "period",
+			regexp: /^\./
+		});
+		this.addTokenType({
+			name: "star",
+			regexp: /^\*/
+		});
+
+		this.addTokenType({
+			name: "comma",
+			regexp: /^,/
+		});
+
+		this.addTokenType({
+			name: "colon",
+			regexp: /^:/
+		});
+    };
 
 	parsing.Lexer.prototype = new canto34.Lexer();
 
